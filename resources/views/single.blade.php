a6c4387a022eb02cecf73ab855ce5cc729652584
jesskorcontrol <jess.rideout@korcontrol.com>
2022-10-18 16:38:16 -0600

    first commit wip

--- resources/views/single.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    @while(have_posts()) @php(the_post())
-    @includeFirst(['content.content-single-' . get_post_type(), 'content.content-single'])
-    @endwhile
-@endsection
